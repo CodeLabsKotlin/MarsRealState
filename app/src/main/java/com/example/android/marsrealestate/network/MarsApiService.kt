732fb3208608330fb5dc298306c73dda0b8f4d72
Tinizara Rodriguez <72140889+TinizaraRodriguez@users.noreply.github.com>
2022-10-25 14:33:53 +0100

    añadimos la opcion para filtrar por compra, venta o mostrar todo

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -23,6 +23,12 @@ import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Query
+
+enum class MarsApiFilter(val value: String) {
+    SHOW_RENT("rent"),
+    SHOW_BUY("buy"),
+    SHOW_ALL("all") }
 
 private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com/"
 
@@ -44,3 +50,5 @@ object MarsApi {
     val retrofitService : MarsApiService by lazy {
         retrofit.create(MarsApiService::class.java) }
 }
+
+suspend fun getProperties(@Query("filter") type: String): List<MarsProperty>
